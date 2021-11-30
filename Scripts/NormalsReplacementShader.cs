93ba208f9825fca3f44aab21350d394aa1aee5d8
Mikey Crowhurst <72895386+mcrowhurst1234@users.noreply.github.com>
2021-11-30 09:40:26 +0000

    Add files via upload

---
+++ Scripts/NormalsReplacementShader.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class NormalsReplacementShader : MonoBehaviour
+{
+    [SerializeField]
+    Shader normalsShader;
+
+    private RenderTexture renderTexture;
+    private new Camera camera;
+
+    private void Start()
+    {
+        Camera thisCamera = GetComponent<Camera>();
+
+        renderTexture = new RenderTexture(thisCamera.pixelWidth, thisCamera.pixelHeight, 24);
+        Shader.SetGlobalTexture("_CameraNormalsTexture", renderTexture);
+
+        GameObject copy = new GameObject("Normals camera");
+        camera = copy.AddComponent<Camera>();
+        camera.CopyFrom(thisCamera);
+        camera.transform.SetParent(transform);
+        camera.targetTexture = renderTexture;
+        camera.SetReplacementShader(normalsShader, "RenderType");
+        camera.depth = thisCamera.depth - 1;
+    }
+}
